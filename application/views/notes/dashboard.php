dcf3402d6fbebae5023721f61485219369e78d47
al3as <wdfathialtayeb@gmail.com>
2017-01-17 09:04:18 -0800

    deploy

--- application/views/notes/dashboard.php
+++ application/views/notes/dashboard.php
@@ -1,7 +1,5 @@
 <div class="header-background font-md text-center">My Notes</div>
 	<div class="col-lg-offset-1 col-lg-10">
-
-	
 		<a class="btn btn-primary btn-block btn-md font-sm" href="<?=base_url('notes/addnotes')?>">
 			 Add notes <span class="glyphicon glyphicon-plus g-font-sm"></span></a>
 		</a>
@@ -24,8 +22,8 @@
 	 	<h3>You have no notes yet.</h3>
 	 <?php endif;?>
 	 <?php foreach($notes as $note):?>
-		<div class="row  margin-top font-sm">
-		<div class="col-lg-6">
+		<div class="row margin-top font-sm">
+		<div class="col-lg-6 col-md-6">
 			<div class="panel panel-primary">
 				<div class="panel-heading">
 					<!-- <?=$i++?> --> <span class="glyphicon glyphicon-book"> </span> <?=$note->notes_name?> 
@@ -35,7 +33,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-lg-6">
+		<div class="col-lg-6 col-md-6">
 			<a href="<?=base_url('notes/done/'. $note->id)?>" class="btn btn-success">Done <span class="glyphicon glyphicon-ok"></a>
 			<a href="<?=base_url('notes/undone/' . $note->id)?>" disabled class="btn btn-warning">UnDone <span class="glyphicon glyphicon-off"></span> </a>
 			<a href="<?=base_url('Notes/edite/' . $note->id)?>" class="btn btn-info">Edit <span class="glyphicon glyphicon-pencil"></a>
